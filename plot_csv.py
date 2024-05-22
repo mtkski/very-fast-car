72d98a4195485de1bfa6e68d80a2b7670d4643e3
AlexBien <alexandre.bienfait@hotmail.com>
2024-05-22 18:22:43 +0200

    last model

--- plot_csv.py
+++ plot_csv.py
@@ -2,26 +2,41 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-TEST_NAME = "alex8"
+TEST_NAME = "final_train"
 
 
 # Read the CSV file
 data = pd.read_csv(f'./save/{TEST_NAME}/Training_log.csv')
 
-# Plot the data
-x_axis = data["epochs"]
-y_axis = data["reward"]
 
-plt.plot(x_axis, y_axis)
+data['time_min'] = data['time_elapsed'] / 60
+# round the time to integer
+data['time_min'] = data['time_min'].round(1)
 
+#transform the memory usage collumn to integer by removing the 'MB' and converting to int
+data['memory_usage'] = data['memory_usage'].str.replace(' MB', '').astype(float)
 
+# Create the base plot
+fig, ax1 = plt.subplots()
 
+# Plot reward on the left y-axis
+ax1.set_xlabel('Epoch')
+ax1.set_ylabel('Reward', color='tab:blue')
+ax1.plot(data['epochs'], data['reward'], color='tab:blue', label='Reward')
+ax1.tick_params(axis='y', labelcolor='tab:blue')
 
+# Create a second y-axis for time in minutes
+ax2 = ax1.twinx()
+ax2.set_ylabel('epsilon', color='tab:green')
+ax2.plot(data['epochs'], data['epsilon'], color='tab:green', label='epsilon')
+ax2.tick_params(axis='y', labelcolor='tab:green')
 
-# Customize the plot if needed
-plt.title('CSV Data Plot')
-plt.xlabel('Episodes')
-plt.ylabel('Total Reward')
+# Add legends
+lines_1, labels_1 = ax1.get_legend_handles_labels()
+lines_2, labels_2 = ax2.get_legend_handles_labels()
+
+ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper left')
 
 # Show the plot
+plt.title('Deep Q-Learning Performance')
 plt.show()
--- train.py
+++ train.py
@@ -4,8 +4,14 @@ import cv2
 from tqdm import tqdm
 from DQNAgent import DQNAgent
 import csv
+from collections import deque
+import psutil
+from memory_profiler import profile
+import time
 
-TEST_NAME = "alex8"
+
+
+TEST_NAME = "final_train"
 NUM_EPOCS = 1000
 STARTING_EPOCH = 0
 BATCH_SIZE = 32
@@ -16,19 +22,20 @@ TRAINING_FREQUENCY = 10
 TARGET_UPDATE_FREQUENCY = 15
 MODEL_SAVE_FREQUENCY = 20
 MIN_FRAME_FOR_NEG_REWARD = 50
+MAX_CONSECUTIVE_NEG_REWARD = 5
 LOADING_EXISTING_MODEL = False
 LOADING_MODEL_PATH = f"./save/{TEST_NAME}/model-220.h5"
 
 
-def save_log(epochs, reward, agent):
+def save_log(epochs, reward, agent, memory_usage, time_elapsed):
     with open(f'./save/{TEST_NAME}/Training_log.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         if epochs % 10 == 0:
             if csvfile.tell() == 0:
-                writer.writerow(["test_name", "epochs", "reward", "epsilon", "epsilon_min", "espilon_decay ", "gamma" , "memory_size" , "BATCH_SIZE size", "STEP_BETWEEN_STATE", "TRAINING_FREQUENCY", "TARGET_UPDATE_FREQUENCY", "MIN_FRAME_FOR_NEG_REWARD"])  # Rename the first columns
-                writer.writerow([TEST_NAME, epochs, reward, agent.epsilon , agent.epsilon_min, agent.epsilon_decay, agent.gamma , agent.memory_size, BATCH_SIZE, STEP_BETWEEN_STATE, TRAINING_FREQUENCY, TARGET_UPDATE_FREQUENCY, MIN_FRAME_FOR_NEG_REWARD])
+                writer.writerow(["test_name", "epochs", "reward", "epsilon", "epsilon_min", "espilon_decay ", "gamma" , "memory_size" , "BATCH_SIZE size", "STEP_BETWEEN_STATE", "TRAINING_FREQUENCY", "TARGET_UPDATE_FREQUENCY", "MIN_FRAME_FOR_NEG_REWARD", "memory_usage", "time_elapsed"])  # Rename the first columns
+                writer.writerow([TEST_NAME, epochs, np.max(reward), agent.epsilon , agent.epsilon_min, agent.epsilon_decay, agent.gamma , agent.memory_size, BATCH_SIZE, STEP_BETWEEN_STATE, TRAINING_FREQUENCY, TARGET_UPDATE_FREQUENCY, MIN_FRAME_FOR_NEG_REWARD, memory_usage, time_elapsed])
         if epochs % 10 == 0:
-            writer.writerow([TEST_NAME, epochs, reward, agent.epsilon , agent.epsilon_min, agent.epsilon_decay, agent.gamma , agent.memory_size, BATCH_SIZE, STEP_BETWEEN_STATE, TRAINING_FREQUENCY, TARGET_UPDATE_FREQUENCY, MIN_FRAME_FOR_NEG_REWARD])
+            writer.writerow([TEST_NAME, epochs, np.max(reward), agent.epsilon , agent.epsilon_min, agent.epsilon_decay, agent.gamma , agent.memory_size, BATCH_SIZE, STEP_BETWEEN_STATE, TRAINING_FREQUENCY, TARGET_UPDATE_FREQUENCY, MIN_FRAME_FOR_NEG_REWARD, memory_usage, time_elapsed])
 
 
 def transform_to_grey_scale(obs):
@@ -36,6 +43,11 @@ def transform_to_grey_scale(obs):
     obs = obs.astype(float)
     obs /= 255.0
     return obs
+
+def memory_usage():
+    process = psutil.Process()
+    mem_info = process.memory_info()
+    return mem_info.rss
 
 if __name__ == "__main__":
     env = gym.make("CarRacing-v2", max_episode_steps=500, domain_randomize=False)
@@ -48,7 +60,9 @@ if __name__ == "__main__":
     
 
 
-
+    last_10_rewards = deque(maxlen=10)
+    
+    start_time = time.time()
     for epoch in tqdm(range(STARTING_EPOCH,NUM_EPOCS)):
         init_frame = env.reset()[0]
         current_frame = transform_to_grey_scale(init_frame)
@@ -80,7 +94,7 @@ if __name__ == "__main__":
             # handle the reward part 
             total_reward += reward
             print(total_reward)
-            if(total_reward < 0 or negative_reward_counter > 5):
+            if(total_reward < 0 or negative_reward_counter > MAX_CONSECUTIVE_NEG_REWARD):
                 break
             
             next_frame = transform_to_grey_scale(next_frame)
@@ -94,7 +108,8 @@ if __name__ == "__main__":
             if len(agent.memory) > BATCH_SIZE and saved_transitions % TRAINING_FREQUENCY == 0:
                 agent.replay(BATCH_SIZE) # train the model with the saved transitions
                 saved_transitions = 0
-            
+        
+        last_10_rewards.append(total_reward)    
         
         if epoch % TARGET_UPDATE_FREQUENCY == 0:
             agent.update_target_model()
@@ -102,7 +117,9 @@ if __name__ == "__main__":
         if epoch % MODEL_SAVE_FREQUENCY == 0:
             agent.save_model(f"./save/{TEST_NAME}/model-{epoch}.h5")
         
-        save_log(epoch, total_reward, agent)
+        memory_usage_string = f"{memory_usage() / (1024 ** 2):.2f} MB"
+        time_elapsed = time.time() - start_time
+        save_log(epoch, last_10_rewards, agent, memory_usage_string, round(time_elapsed, 1))
                             
            
     env.close()
